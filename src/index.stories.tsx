f8b07404a0d582be6a642273cff8953cd845f44b
Ricardo Alexis Quiroz Bazan <raquirozbazan@gmail.com>
2019-09-03 14:54:46 -0500

    chore: support modify notifier options in the story 👌

--- src/index.stories.tsx
+++ src/index.stories.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { text, select } from '@storybook/addon-knobs'
+import { text, select, number } from '@storybook/addon-knobs'
 import { INotification, IMountOptions } from './types'
 import notifier from './index'
 
@@ -10,34 +10,52 @@ export default { title: 'notifier' }
  * by HRM in development mode
  */
 const useNotifier = (options: IMountOptions) => {
+  const dependencies = Object.keys(options).map(
+    (key: keyof IMountOptions) => options[key]
+  )
+
   React.useEffect(() => {
     notifier.mount(options)
 
     return () => {
       notifier.unmount()
     }
-  }, [])
+  }, [dependencies])
 }
 
-const DemoApp: React.FC<INotification> = props => {
-  const { message, kind } = props
+type Props = {
+  notification: INotification
+  options: IMountOptions
+}
 
-  useNotifier({
-    timeout: 3 * 1000,
-    interval: 300,
-    capacity: 3
-  })
+const DemoApp: React.FC<Props> = props => {
+  const { notification, options } = props
+
+  useNotifier(options)
 
   const notify = () => {
-    notifier.notify({ message, kind })
+    notifier.notify(notification)
   }
 
   return <button onClick={notify}>Show</button>
 }
 
 export const Normal = () => {
-  const message = text('message', 'Hello World!')
-  const kind = select('kind', ['info', 'error', 'success', 'warning'], 'info')
+  const notification = {
+    message: text('message', 'Hello World!', 'notification'),
+    kind: select(
+      'kind',
+      ['info', 'error', 'success', 'warning'],
+      'info',
+      'notification'
+    )
+  }
 
-  return <DemoApp message={message} kind={kind} />
+  const options = {
+    timeout: number('timeout', 3 * 1000, undefined, 'notifier'),
+    interval: number('interval', 3 * 100, undefined, 'notifier'),
+    capacity: number('capacity', 3, undefined, 'notifier')
+  }
+
+  return <DemoApp notification={notification} options={options} />
 }
