d541ded72e1e612d32fa7bda4ef5f588398ce9bb
Ricardo Alexis Quiroz Bazan <raquirozbazan@gmail.com>
2019-12-21 14:50:21 -0500

    build: use @rollup/plugin-typescript

--- packages/library/rollup.config.js
+++ packages/library/rollup.config.js
@@ -1,5 +1,5 @@
 import dts from 'rollup-plugin-dts'
-import typescript from 'rollup-plugin-typescript'
+import typescript from '@rollup/plugin-typescript'
 import pkg from './package.json'
 
 export default [
